341830e8ff4fec245aff8de9e44a38b8a46fe04e
evgeniy <evgeniy@takeit.to>
2015-11-27 23:26:01 +0200

    setup db config

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -16,11 +16,25 @@ hibernate {
 
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+    dataSource {
+        driverClassName = "org.postgresql.Driver"
+        dialect = "org.hibernate.dialect.PostgreSQLDialect"
+        pooled = true
+        type = "com.mchange.v2.c3p0.ComboPooledDataSource"
+        properties {
+            maxActive = -1
+            minEvictableIdleTimeMillis=1800000
+            timeBetweenEvictionRunsMillis=1800000
+            numTestsPerEvictionRun=3
+            testOnBorrow=true
+            testWhileIdle=true
+            testOnReturn=true
+            validationQuery="SELECT 1"
         }
+        username = 'postgres'
+        password = '123'
+        url = "jdbc:postgresql://localhost:5432/uwcuafinal"
+        dbCreate = "create-drop"
     }
     test {
         dataSource {
@@ -30,28 +44,30 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dbCreate = "create-drop"
+            username = 'rowsbjchsrhsth'
+            password = '6FU4H21wXbnhdDhuYt2jkrZuGc'
+            url = "postgres://rowsbjchsrhsth:6FU4H21wXbnhdDhuYt2jkrZuGc@ec2-54-204-35-248.compute-1.amazonaws.com:5432/do8dq3for698k"
             properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+                jmxEnabled = true
+                initialSize = 5
+                maxActive = 50
+                minIdle = 5
+                maxIdle = 25
+                maxWait = 10000
+                maxAge = 10 * 60000
+                timeBetweenEvictionRunsMillis = 5000
+                minEvictableIdleTimeMillis = 60000
+                validationQuery = "SELECT 1"
+                validationQueryTimeout = 3
+                validationInterval = 15000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                jdbcInterceptors = "ConnectionState"
+                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
             }
         }
     }
+
 }
