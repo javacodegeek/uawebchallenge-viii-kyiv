8beec9dfe412de0404b5f1697548d36e8d5fce66
evgeniy <evgeniy@takeit.to>
2015-11-28 18:19:30 +0200

    setup bootstrap

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -44,6 +44,8 @@ environments {
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
+
+
     production {
         dataSource {
             dbCreate = "create-drop"
